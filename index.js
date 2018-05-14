c59f70fc58785225a100f231bf67c53630adeb73
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-14 20:06:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function calculatesFarePrice(start, end) {
   if (length < 400) {
       return 0 }
   else if (length >= 400 && length <= 2000) {
-      return length*.02 }
+      return length *.02 }
   else if (length > 2000 && length <= 2500) {
       return 25 }
   else {'cannot travel that far'}
